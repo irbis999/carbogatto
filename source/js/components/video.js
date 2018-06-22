7cfda4b22cd89b6d5db7d2163541fdd08b16f8fa
Alexey Tikhonenko <webmaster@maximumufa.ru>
2018-06-22 19:11:54 +0500

    comments

--- source/js/components/video.js
+++ source/js/components/video.js
@@ -1,29 +1,37 @@
 import _debounce from 'lodash.debounce'
+import device from 'current-device'
+
 
 class Video {
-	constructor(options) {
-		this.elem = options.elem
-		this.videoElems = this.elem.find('video')
-		this.mup = $('body').data('mup')
-		this.inited = false
-		//Изначально у видео нет атрибута src, чтобы они не грузились на моб версии
-		this.checkVideo()
-		$(window).on('resize', _debounce(this.checkVideo.bind(this), 400))
-	}
+  constructor(options) {
+    this.elem = options.elem
+    this.videoElems = this.elem.find('video')
+    //mup = medium up - число пикселов когда мобильная версия превращатеся в десктопную
+    this.mup = $('body').data('mup')
+    this.inited = false
+    //Изначально у видео нет атрибута src, чтобы они не грузились на моб версии, есть атрибут data-src
+    //когда
+    this.checkVideo()
+    $(window).on('resize', _debounce(this.checkVideo.bind(this), 400))
+  }
 
-	checkVideo() {
-		if(this.inited) return
-		if(window.innerWidth < this.mup) return
-		this.videoElems.each((index, elem) => {
-			$(elem).attr('src', $(elem).data('src'))
-			$(elem).attr('poster', $(elem).data('poster'))
-		})
-		this.inited = true
-	}
+  checkVideo() {
+    if (this.inited) return
+    if (window.innerWidth < this.mup) return
+    this.videoElems.each((index, elem) => {
+      //На всех широких устройствах покажем постер
+      $(elem).attr('poster', $(elem).data('poster'))
+      if (device.desktop()) {
+        //На широких десктопах покажем само видео
+        $(elem).attr('src', $(elem).data('src'))
+      }
+    })
+    this.inited = true
+  }
 }
 
 $(document).ready(() => {
-	$(document).each((index, elem) => {
-		new Video({elem: $(elem)})
-	})
+  $(document).each((index, elem) => {
+    new Video({elem: $(elem)})
+  })
 })
